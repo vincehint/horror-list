21ff668c9649dcccc8cfa5872759c3f4658919cc
Vince Hintermeister <vv.hintermeister@gmail.com>
2021-01-22 15:59:36 -0600

    trying to get the search to work

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const passport = require('./config/ppConfig.js')
 // bring in flash
 const flash = require('connect-flash')
 const isLoggedIn = require('./middleware/isLoggedIn.js')
+const API_KEY = process.env.API_KEY;
 //middleware to create different html files
 app.set('view engine', 'ejs');     
 //enable layouts
@@ -49,6 +50,22 @@ app.get('/profile', isLoggedIn, (req, res) => {
 app.get('*', (req, res) => {
     res.render('./404.ejs')
 })
+
+app.get('/results', (req, res) => {
+    axios.get(`https://api.themoviedb.org/3/discover/movie?api_key=b5e71871c61ba9cafb3c1f7fcf1243ec&with_genres=27`)
+    .then(response => {
+      console.log(response)
+      console.log(req, res)
+      res.render('results', {movies: response.data.Search});
+  });
+  })
+  
+  app.get('/results/:id', (req, res) => {
+    axios.get(`https://api.themoviedb.org/3/movie/550?api_key=${API_KEY}&i=${req.params.id}&with_genres=27`)
+    .then(response => {
+      res.render('show', {movie: response.data})
+    });
+  })
 
 
 app.listen(process.env.PORT, () => {
